4d639c6e22e47e745e34cb5c66968dfd6cf75bce
Luc1100 <52473174+Luc1100@users.noreply.github.com>
2022-07-06 20:53:36 -0400

    Show selected color and color pixel borders

--- javascript.js
+++ javascript.js
@@ -1,6 +1,4 @@
 
-let currColor = 'black';
-
 function setupContainer(size) {
     const container = document.querySelector('.container')
 
@@ -18,37 +16,40 @@ function setupContainer(size) {
 
     pixels.forEach(pixel => {
         pixel.addEventListener('mouseover', (e) => {
-            let transparency = false;
-            if (transparency) {
-                addAlpha(e.target);
-            } else {
-                e.target.style.backgroundColor = currColor;
-            }
+            e.target.style.backgroundColor = currColor;
+            e.target.style.borderColor = currColor;
         });
     });
 }
-
-function addAlpha(target) {
-    let currentBGC = target.style.backgroundColor;
-    currentBGC = currentBGC.split(", ");
-    console.log(currentBGC);
-
-    let alpha = currentBGC[currentBGC.length - 1];
-    alpha = parseFloat(alpha.substring(0, [alpha.length - 1]));
-    
-    let newAlpha = alpha + 0.10;
-
-    if (newAlpha < 1) {
-        target.style.backgroundColor = `rgba(0, 0, 0, ${newAlpha})`;
-    }
-} 
 
 function clearContainer() {
     const container = document.querySelector('.container');
     container.innerHTML = '';
 }
 
-setupContainer(16);
+function selectColor(target) {
+    const colorBtns = document.querySelectorAll('.btn');
+    colorBtns.forEach(btn => {
+        btn.classList.remove('selected');
+    });
+    target.classList.add('selected');
+}
+
+// Function to color pixel initially with alpha 0.1 and continue adding 0.1 until full black reached.
+// function addAlpha(target) {
+//     let currentBGC = target.style.backgroundColor;
+//     currentBGC = currentBGC.split(", ");
+//     console.log(currentBGC);
+
+//     let alpha = currentBGC[currentBGC.length - 1];
+//     alpha = parseFloat(alpha.substring(0, [alpha.length - 1]));
+    
+//     let newAlpha = alpha + 0.10;
+
+//     if (newAlpha < 1) {
+//         target.style.backgroundColor = `rgba(0, 0, 0, ${newAlpha})`;
+//     }
+// } 
 
 const pixelSlider = document.querySelector('.pixel-slider');
 const slideValue = document.querySelector('.slide-value');
@@ -60,30 +61,40 @@ pixelSlider.oninput = () => {
 }
 
 const redBtn = document.querySelector('.red');
-redBtn.addEventListener('click', () => {
+redBtn.addEventListener('click', (e) => {
     currColor = '#FF6663';
+    selectColor(e.target);
 });
 const orangeBtn = document.querySelector('.orange');
-orangeBtn.addEventListener('click', () => {
+orangeBtn.addEventListener('click', (e) => {
     currColor = '#FEB144';
+    selectColor(e.target);
 });
 const yellowBtn = document.querySelector('.yellow');
-yellowBtn.addEventListener('click', () => {
+yellowBtn.addEventListener('click', (e) => {
     currColor = '#FDFD97';
+    selectColor(e.target);
 });
 const greenBtn = document.querySelector('.green');
-greenBtn.addEventListener('click', () => {
+greenBtn.addEventListener('click', (e) => {
     currColor = '#9EE09E';
+    selectColor(e.target);
 });
 const blueBtn = document.querySelector('.blue');
-blueBtn.addEventListener('click', () => {
+blueBtn.addEventListener('click', (e) => {
     currColor = '#9EC1CF';
+    selectColor(e.target);
 });
 const purpleBtn = document.querySelector('.purple');
-purpleBtn.addEventListener('click', () => {
+purpleBtn.addEventListener('click', (e) => {
     currColor = '#CC99C9';
+    selectColor(e.target);
 });
 const blackBtn = document.querySelector('.black');
-blackBtn.addEventListener('click', () => {
+blackBtn.addEventListener('click', (e) => {
     currColor = 'black';
+    selectColor(e.target);
 });
+
+let currColor = 'black';
+setupContainer(16);
